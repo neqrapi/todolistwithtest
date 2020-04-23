c78f75f16be291c37ff8002d467ba3df78370c2b
neqrapi <60574161+neqrapi@users.noreply.github.com>
2020-04-23 18:50:29 +0200

    a

--- src/Todos.test.js
+++ src/Todos.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {shallow} from enzyme;
+import {shallow} from 'enzyme';
 import {Todos} from './Todos'
 it("should render You have no todo's left, yay!",()=>{
     const wrapper=shallow(<Todos/>)
-    const span=wrapper.find('span')
+    const p=wrapper.find('p.center').text()
     const resualt=span.text()
     expect(resualt).toBe('You have no todos left, yay!')
 })
